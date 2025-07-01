923baecf04b94bf9da39a6a00ea711e546a0ab61
Bilal Hameed <77564057+BilalHameed22@users.noreply.github.com>
2025-07-01 22:43:17 +0500

    Client module created.

---
+++ client/target/debug/deps/client-7d7e40c70bc34972.d
@@ -0,0 +1,5 @@
+/home/bilal/Desktop/Blockchain/RUST/Projects/chat/client/target/debug/deps/libclient-7d7e40c70bc34972.rmeta: src/main.rs
+
+/home/bilal/Desktop/Blockchain/RUST/Projects/chat/client/target/debug/deps/client-7d7e40c70bc34972.d: src/main.rs
+
+src/main.rs:
---
+++ client/target/debug/deps/client-be6710aa32e7cb24.d
@@ -0,0 +1,5 @@
+/home/bilal/Desktop/Blockchain/RUST/Projects/chat/client/target/debug/deps/libclient-be6710aa32e7cb24.rmeta: src/main.rs
+
+/home/bilal/Desktop/Blockchain/RUST/Projects/chat/client/target/debug/deps/client-be6710aa32e7cb24.d: src/main.rs
+
+src/main.rs:
